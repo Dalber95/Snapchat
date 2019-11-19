72ea35bde4e2aa83e033c31921ca38d79aa3558d
Kamil <krusiec.kamil82@gmail.com>
2019-11-19 13:24:13 +0000

    Delete snaps from database

--- app/src/main/java/pl/krusiec/snapchatjava/ViewSnapActivity.java
+++ app/src/main/java/pl/krusiec/snapchatjava/ViewSnapActivity.java
@@ -2,14 +2,26 @@ package pl.krusiec.snapchatjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ViewSnapActivity extends AppCompatActivity {
 
     TextView messageTextView;
     ImageView snapImageView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,42 @@ public class ViewSnapActivity extends AppCompatActivity {
         snapImageView = findViewById(R.id.snapImageView);
 
         messageTextView.setText(getIntent().getStringExtra("message"));
+
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute(getIntent().getStringExtra("imageURL")).get();
+            snapImageView.setImageBitmap(myImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                return BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        FirebaseDatabase.getInstance().getReference().child("users").child(mAuth.getCurrentUser().getUid()).child("snaps")
+                .child(getIntent().getStringExtra("snapKey")).removeValue();
+
+        FirebaseStorage.getInstance().getReference().child("images").child(getIntent().getStringExtra("imageName")).delete();
     }
 }
